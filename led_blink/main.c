e0af77279beaffe5fd515b82d0bea0666a246052
shrujan0 <199628475+shrujan0@users.noreply.github.com>
2026-03-29 17:01:43 +0530

    Main program and header files

---
+++ led_blink/main.c
@@ -0,0 +1,34 @@
+#include "stm32f411xe.h"
+
+#define GPIOCEN (1U << 2) //3rd bit to enable GPIO
+#define LED_PIN (1U << 13)
+
+void led_init(void) {
+    //
+    RCC->AHB1ENR |= GPIOCEN;
+
+    //Set led pin as output
+    GPIOC->MODER |= (1U << 26);
+    GPIOC->MODER &= ~(1U << 27);
+}
+
+void led_on(void) {
+    //set led pin HIGH (PC13)
+    GPIOC->ODR |= LED_PIN;
+}
+
+void led_off(void) {
+    //set led pin low (PC13)
+    GPIOC->ODR &= ~LED_PIN;
+}
+
+int main(void) {
+    led_init();
+
+    while(1) {
+        led_on();
+        for (int i = 0; i < 1000000; i++) {}
+        led_off();
+        for (int i = 0; i < 1000000; i++) {}
+    }
+}
